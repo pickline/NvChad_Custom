18ad4a449c844ba4e83bc58ebf84396cb1d845c3
pickline <adcmasters@outlook.com>
2023-07-06 09:17:09 +0800

    add close buffer keybinding

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,7 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-s>"] = { "<CMD> w <CR>", "save file", opts = { nowait = true } },
     ["<C-x>"] = { "<CMD> q <CR>", "quit", opts = { nowait = true } },
+    ["<C-b>"] = { "<CMD> bd<CR>", "close buffer", opts = { nowait = true } },
   },
   i = {
     ["<C-s>"] = { "<CMD> w <CR>", "save file", opts = { nowait = true } },
